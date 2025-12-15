55027a6f9f7e46b260026373b42c45df41912642
Abdullah almousa <abdullahmlwork@gmail.com>
2025-12-15 18:51:22 +0300

    Trying To Give Output For The Edited Meme

--- composeApp/src/androidMain/kotlin/com/mysticbyte/memecreatorapp/meme_editor/data/PlatformMemeExporter.android.kt
+++ composeApp/src/androidMain/kotlin/com/mysticbyte/memecreatorapp/meme_editor/data/PlatformMemeExporter.android.kt
@@ -1,14 +1,40 @@
 package com.mysticbyte.memecreatorapp.meme_editor.data
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
+
 import androidx.compose.ui.unit.IntSize
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.withTranslation
+
 import com.mysticbyte.memecreatorapp.meme_editor.domain.MemeExporter
 import com.mysticbyte.memecreatorapp.meme_editor.domain.SaveToStorageStrategy
 import com.mysticbyte.memecreatorapp.meme_editor.presentation.MemeText
+import com.mysticbyte.memecreatorapp.meme_editor.presentation.util.MemeRenderCalculator
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.withContext
+
+import java.io.File
+import java.io.FileOutputStream
 
 actual class PlatformMemeExporter(
     private val context: Context
 ) : MemeExporter {
+
+    private val memeRenderer = MemeRenderCalculator(
+        density = context.resources.displayMetrics.density
+    )
+
     actual override suspend fun exportMeme(
         backgroundImageByte: ByteArray,
         memeTexts: List<MemeText>,
@@ -16,5 +42,9 @@ actual class PlatformMemeExporter(
         saveToStorageStrategy: SaveToStorageStrategy,
         fileName: String
     ) {
+
+        var bitmap: Bitmap? = null
+        var outputBitmap: Bitmap? = null
+
     }
 }
